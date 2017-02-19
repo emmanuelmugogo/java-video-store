0412e91a9f99b432308c20085ad63a92c1b8aecf
emmanuelmugogo <mugoclan87@gmail.com>
2017-02-19 04:15:27 -0500

    can write toa text file VideoRecords.txt

--- src/main/java/RentalStatement.java
+++ src/main/java/RentalStatement.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -13,6 +16,25 @@ public class RentalStatement {
 			System.out.println("\nprice code: " + rental.getPriceCode() +
 					 "\nTitle - " + rental.getTitle() + "\nFees - $" + rental.calculateFees() + "\n");
 		}
+		
+	}
+	
+	public void printToText() throws Exception {
+		
+		FileOutputStream fileOutput = new FileOutputStream("VideoRecords.txt");
+		PrintStream printOutput = new PrintStream(fileOutput);
+		
+		printOutput.println("Collection of rented movie...");
+		
+		for (Rental rental : rentals) {
+			
+			printOutput.println("\nprice code: " + rental.getPriceCode() +
+					 "\nTitle - " + rental.getTitle() + "\nFees - $" + rental.calculateFees() + "\n");
+			
+		}
+		
+		fileOutput.close();
+		printOutput.close();
 		
 	}
 
--- src/main/java/VideoStoreApp.java
+++ src/main/java/VideoStoreApp.java
@@ -10,9 +10,9 @@ public class VideoStoreApp {
 		Movie childrens = new ChildrensMovie("Up");
 		Movie newRelease = new NewRelease("Dr Strange");
 		
-		Rental regualarMovie = new Rental(regular, 5);
-		Rental childrensMovie = new Rental(childrens, 5);
-		Rental newReleaseMovie = new Rental(newRelease, 5);
+		Rental regualarMovie = new Rental(regular, 3);
+		Rental childrensMovie = new Rental(childrens, 3);
+		Rental newReleaseMovie = new Rental(newRelease, 3);
 		
 		statement.add(regualarMovie);
 		statement.add(childrensMovie);
@@ -20,6 +20,13 @@ public class VideoStoreApp {
 
 		System.out.println("Movies rented for 5 days");
 		statement.print();
+		
+		//this will print to the text file called videoRecords.txt
+		try {
+			statement.printToText();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
